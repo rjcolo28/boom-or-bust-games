b24c0947e7d82750362602c406e4d551343798d9
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-04-13 13:32:40 -0400

    Added mongoose and routes to server + built backendfile structure

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/games-reloaded";
+
+mongoose.connect(MONGODB_URI)
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
