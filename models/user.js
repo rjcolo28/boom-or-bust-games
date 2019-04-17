b1497cfa54bb18fcca29e760b5d400e1e54df5d3
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-04-16 20:42:51 -0400

    Set up schemas and added axios npm

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema ({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true, min: 6 },
+    email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, "Please enter a valid e-mail address"] },
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
