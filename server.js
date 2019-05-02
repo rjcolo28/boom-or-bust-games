b7c692859364c7c3fd6429c561f94f72b968ed16
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-05-02 14:00:04 -0400

    db-backup

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 var logger = require("morgan");
+const path = require("path");
 const port = process.env.PORT || 5000;
 require("dotenv").config()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use(logger("dev"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("/client/build"));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // console.log that server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
